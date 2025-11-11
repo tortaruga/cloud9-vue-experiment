1c7b0182274a0e3709b17c00d3462dcd38627012
tortaruga <jojoway77@gmail.com>
2025-11-11 16:00:51 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+   hello world! this will be a website for a fictional travelling agency, made to practice Vue. 
+   it's my first vue project, so i expect it will probably suck.
+   let's go!
+  </div>
+</template>
+
+<style scoped>
+</style>
